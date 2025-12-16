6097f782f4e39c65d7772fb532135190762a77eb
Vik8126 <aaravrai931@gmail.com>
2025-12-16 12:19:58 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: "/", // ✅ IMPORTANT
+  build: {
+    outDir: "dist", // ✅ Netlify expects this
+    assetsDir: "assets",
+    sourcemap: false,
+  },
 });
